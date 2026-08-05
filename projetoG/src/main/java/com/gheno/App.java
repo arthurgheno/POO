f387b4c6f00a57e8b2d0fcdaba33fddb76c261e0
arthur.gheno@edu.pucrs.br <arthur.gheno@edu.pucrs.br>
2026-08-05 13:34:48 +0000

    Olá

---
+++ projetoG/src/main/java/com/gheno/App.java
@@ -0,0 +1,10 @@
+package com.gheno;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(
+            "Hello, world! Projeto projetoG criado com sucesso."
+        );
+    }
+}
