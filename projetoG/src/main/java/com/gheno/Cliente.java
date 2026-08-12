aaceac98a0a4fb97c261f5ce0bf2fe9c530bcf5e
arthur.gheno@edu.pucrs.br <arthur.gheno@edu.pucrs.br>
2026-08-12 14:03:22 +0000

    xx3

--- projetoG/src/main/java/com/gheno/Cliente.java
+++ projetoG/src/main/java/com/gheno/Cliente.java
@@ -1,8 +1,9 @@
+package com.gheno;
 public class Cliente{
   private String nome;
   private String cpf;
   private int pontos;
-  private double valor
+  private double valor;
 
   public Cliente(){
     nome="";
@@ -11,11 +12,17 @@ public class Cliente{
     valor=0;
   }
 
-  public Cliente(String nome, String cpf, int pontos){
+  public Cliente(String nome, String cpf){
     this.nome = nome;
     this.cpf = cpf;
-    this.pontos = pontos;
+    this.pontos = 0;
+    this.valor = 0;
   }
+  public Cliente(String nome, String cpf, int pontos) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.pontos = pontos;
+    }
 
   public String getNome() {
     return nome;
@@ -35,16 +42,49 @@ public class Cliente{
     return valor;
   }
 
-  public double calculaPontos(int pontos, double valor){
-    if(valor<500){
-        valor = valor/100;
+  public int calculaPontos(int pontos, double valor) {
+    if (valor < 500) {
+        valor = valor / 100;
         valor = Math.round(valor);
+    } else {
+        valor = valor / 500;
+        valor = Math.round(valor);
+        valor = valor * 5;
+    }
+    pontos = (int) valor;
+    return pontos;
+}
+  public double calculaDesconto(double valor){
+    if(pontos<10){
+        System.out.println("Sem desconto");
+        return valor;
+    }
+    else if(pontos>=10 && pontos<50){
+        return valor-(valor*0.10);
     }
     else{
-        valor = valor/500;
-        Math.round(valor);
-        valor = valor*5;
+        return valor-(valor*0.20);
     }
   }
-
+  public double registrarCompra(int opcao, double valor){
+    if(opcao == 1){
+        int pontosGanhos = calculaPontos(pontos,valor);
+        this.pontos += pontosGanhos;
+        return valor;
+    }
+    else if(opcao == 2){
+        if(pontos<10){
+            System.out.println("Pontos não suficientes para desconto");
+            return valor;
+        }
+    else{
+        double valorComDesconto = calculaDesconto(valor);
+        this.pontos -=10;
+        return valorComDesconto;
+    }
+  }
+  else{
+    throw new IllegalArgumentException("Opção Inválida");
+  }
+}
 }
---
+++ projetoG/src/main/java/com/gheno/ClienteApp.java
@@ -0,0 +1,38 @@
+package com.gheno;
+
+import java.util.Scanner;
+
+public class ClienteApp {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String nome, cpf;
+        double valor;
+        int opcao;
+
+        System.out.println("Digite o nome do cliente: ");
+        nome = in.nextLine();
+
+        System.out.println("Digite o CPF do cliente: ");
+        cpf = in.nextLine();
+
+        Cliente c = new Cliente(nome,cpf);
+
+        System.out.println("Digite o valor da compra: ");
+        valor = in.nextDouble();
+
+        System.out.println("Digite a opção desejada: ");
+        System.out.println("1 - Acumular pontos");
+        System.out.println("2 - Usar o desconto");
+        opcao = in.nextInt();
+
+        double valorFinal = c.registrarCompra(opcao, valor);
+
+        System.out.println("Nome: " + c.getNome());
+        System.out.println("CPF: " + c.getCpf());
+        System.out.println("Pontuação atual: " + c.getPontos());
+        System.out.println("Valor a pagar: " + valorFinal);
+
+    }
+}
